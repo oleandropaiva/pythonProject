477d30a38d328cedbba57df6da53f63495600327
Leandro Paiva <leandroapaiva@yahoo.com>
2022-10-07 10:18:21 -0300

    exemplo

--- aula10/aula10.py
+++ aula10/aula10.py
@@ -6,10 +6,11 @@ idade = input('Qual a sua idade? ')
 
 idade = int(idade)
 #  Limite para pegar um empréstimo
-idade_limite = 18
+idade_menor = 20
+idade_maior = 30
 
 
-if idade >= idade_limite:
+if idade >= idade_menor and idade <= idade_maior:
     print(f'{nome} PODE pegar o empréstimo.')
 else:
     print(f'{nome} NÃO pode pegar o empréstimo.')

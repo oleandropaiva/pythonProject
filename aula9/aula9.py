34c24959df9db1595572bb882e67ef196c2d4bdc
Leandro Paiva <leandroapaiva@yahoo.com>
2022-10-06 21:33:38 -0300

    if

---
+++ aula9/aula9.py
@@ -0,0 +1,11 @@
+# Condições IF, ELIF e ELSE
+
+if True:
+    print('Verdadeiro')
+    num_1 = 2
+    num_2 = 4
+    print(num_1 + num_2)
+
+    if False:
+        print('Verdadeiro')
+    print('A minha expressão não é verdadeira')

18f48a21e5d0360bd53c0548e32a94ce653d6f93
Leandro Paiva <leandroapaiva@yahoo.com>
2022-10-06 21:42:41 -0300

    elif

--- aula9/aula9.py
+++ aula9/aula9.py
@@ -1,6 +1,11 @@
 # Condições IF, ELIF e ELSE
 
+
 if False:
     print("Verdadeiro")
+elif False:
+    print("Agora é verdadeiro.")
+elif False:
+    print("Mais uma verdadeira")
 else:
     print("Não é verdadeiro.")

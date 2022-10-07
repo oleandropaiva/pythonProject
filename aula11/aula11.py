68bba311d90cf05ee61af39f8a61ff21afdfdf0b
Leandro Paiva <leandroapaiva@yahoo.com>
2022-10-07 10:42:54 -0300

    Operadores Lógicos

---
+++ aula11/aula11.py
@@ -0,0 +1,26 @@
+"""
+Operadores Lógicos
+and, or, not,
+in e not in
+"""
+#comparacao1 and comparacao2
+# (True E False) = False
+# (True E True) = True
+# (False E False) = True
+
+#comparacao1 or comparacao2
+# (True OR False) = True
+# (True OR True) = True
+# (False OR False) = False
+
+# NOT - inverte o valor
+a = 2
+b = 3
+
+if not b > a:
+    print('B é maior que A')
+else:
+    print('A é maior do que B.')
+
+
+

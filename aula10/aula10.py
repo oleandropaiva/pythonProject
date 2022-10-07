9f5a407df96a223905690cbcb9b9819d992003d1
Leandro Paiva <leandroapaiva@yahoo.com>
2022-10-07 09:43:36 -0300

    operador de exempo maior ou igual

--- aula10/aula10.py
+++ aula10/aula10.py
@@ -1,9 +1,15 @@
 # Operadores relacionais
 # == > >= < <= !=
 
-num_1 = 3 #int
-num_2 = 1 #int
+nome = input('Qual o seu nome? ')
+idade = input('Qual a sua idade? ')
 
-expressao = (num_2 < num_1)
+idade = int(idade)
+#  Limite para pegar um empréstimo
+idade_limite = 18
 
-print(expressao)
+
+if idade >= idade_limite:
+    print(f'{nome} PODE pegar o empréstimo.')
+else:
+    print(f'{nome} NÃO pode pegar o empréstimo.')

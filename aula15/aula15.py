472769018dcf79a8460a019851d79c635e4c423e
Leandro Paiva <leandroapaiva@yahoo.com>
2022-10-13 21:16:44 -0300

    descrição dos exemplos

---
+++ aula15/aula15.py
@@ -0,0 +1,13 @@
+"""
+Formatando valores com modificadores
+
+:s - Texto (strings)
+:d - Inteiros (int)
+:f - Números de ponto flutuante (float)
+:. (NÚMERO)f - Quantidade de casas decimais (float)
+: (CARACTERE)(> ou < ou ^)(QUANTIDADE)(TIPO - s, d OU f)
+
+> - Esquerda
+< - Direita
+^  - Centro
+"""

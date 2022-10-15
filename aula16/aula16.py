1ee06eed8a81b6d929383aa55acdc17e5c2e1359
Leandro Paiva <leandroapaiva@yahoo.com>
2022-10-14 21:35:03 -0300

    aula16

---
+++ aula16/aula16.py
@@ -0,0 +1,12 @@
+"""
+Manipulando strings
+* String indices
+* Fatiamento de strings [inicio: fim: passo]
+* Funções built-in len, abs, type, print, etc...
+Essas funções podem ser usadas diretamente em cada tipo.
+
+Você pode conferir tudo isso em:
+https://docs.python.org/3/library/stdtypes.html (tipos built-in)
+https://docs.python.org/3/library/stdtypes.html (funções built-in)
+"""
+

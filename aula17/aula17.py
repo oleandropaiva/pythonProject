fac8310465f49489d8519856efa22cbf891b481d
Leandro Paiva <leandroapaiva@yahoo.com>
2022-10-15 19:49:17 -0300

    loop infinito while

---
+++ aula17/aula17.py
@@ -0,0 +1,13 @@
+"""
+while em Python
+utilizadopara realizar ações emquanto
+uma condição for verdadeira.
+
+Requisitos: Entender condições e operadores
+"""
+while True:
+    nome = input("Qual o seu nome? ")
+    print(f'Olá {nome}!')
+
+print("Não será executada")
+

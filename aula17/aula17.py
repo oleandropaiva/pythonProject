ee83d4223b5466da8de53dcd967bde60f331031d
Leandro Paiva <leandroapaiva@yahoo.com>
2022-10-20 20:53:50 -0300

    testando cod

--- aula17/aula17.py
+++ aula17/aula17.py
@@ -4,15 +4,13 @@ utilizadopara realizar ações emquanto
 uma condição for verdadeira.
 Requisitos: Entender condições e operadores
 """
-x = 0
+x = 0  # coluna
+y = 0  # linha
 while x < 10:
-    if x == 3:
-            x = x + 1
-            break
-
     print(x)
-    x = x + 1
+    x += 1  # x = x + 1
 
-print('Acabou!')
+print('Acabou')
+
 
 

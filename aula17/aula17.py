ca5123890a252762c553aaf9ab47e230e935f471
Leandro Paiva <leandroapaiva@yahoo.com>
2022-10-20 20:59:12 -0300

    looping while

--- aula17/aula17.py
+++ aula17/aula17.py
@@ -2,12 +2,18 @@
 while em Python
 utilizadopara realizar ações emquanto
 uma condição for verdadeira.
+
 Requisitos: Entender condições e operadores
 """
+
 x = 0  # coluna
-y = 0  # linha
 while x < 10:
-    print(x)
+    y = 0  # linha
+
+    while y < 5:
+        print(f'X vale {x} e Y vale {y}')
+        y += 1
+
     x += 1  # x = x + 1
 
 print('Acabou')

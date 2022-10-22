9d30562be846a41fb2460eac8e782c0b065c01bf
Leandro Paiva <leandroapaiva@yahoo.com>
2022-10-22 17:05:15 -0300

    Iterando strings com while em Python

---
+++ aula19/aula19.py
@@ -0,0 +1,10 @@
+#        Índices
+#        0123456789........33
+
+frase = 'o rato roeu a roupa do rei de roma'
+tamanho_frase = len(frase)
+contador = 0
+
+while contador < 10:
+    print(contador)
+    contador += 1

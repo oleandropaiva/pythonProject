128719209a6ffca7d7b93d64914b50a7e25a4721
Leandro Paiva <leandroapaiva@yahoo.com>
2022-10-22 18:08:03 -0300

    exemplo_2 if e else

--- aula19/aula19.py
+++ aula19/aula19.py
@@ -7,9 +7,11 @@ contador = 0
 nova_string = ''
 
 while contador < tamanho_frase:
-    # print(frase[contador], contador)
-    nova_string += frase[contador]
-    print(nova_string)
+    letra = frase[contador]
+    if letra == 'r':
+        nova_string += 'R'
+    else:
+        nova_string += letra
     contador += 1
 
 print(nova_string)

2fe30dd573a420e822792df1999c7f52a5406f8f
Leandro Paiva <leandroapaiva@yahoo.com>
2022-10-22 18:37:40 -0300

    exe1

--- aula20/aula20.py
+++ aula20/aula20.py
@@ -6,3 +6,7 @@ Função range(start=0, stop, step=1)range recebe 3 argumentos
 """
 for numero in range(20, 10, -1):
     print(numero)
+
+for numero in range(100):
+    if numero % 8 == 0:
+        print(numero)

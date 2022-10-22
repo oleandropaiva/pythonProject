68c1e0ea3969deb50e99b8b5ec310db7cb0087a3
Leandro Paiva <leandroapaiva@yahoo.com>
2022-10-22 18:02:51 -0300

    iretando exemplo

--- aula19/aula19.py
+++ aula19/aula19.py
@@ -4,7 +4,12 @@
 frase = 'o rato roeu a roupa do rei de roma'
 tamanho_frase = len(frase)
 contador = 0
+nova_string = ''
 
 while contador < tamanho_frase:
-    print(frase[contador], contador)
+    # print(frase[contador], contador)
+    nova_string += frase[contador]
+    print(nova_string)
     contador += 1
+
+print(nova_string)

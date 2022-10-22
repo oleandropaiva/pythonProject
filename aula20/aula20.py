7f52b71968beee3201e8aca214df3e4dc8396b35
Leandro Paiva <leandroapaiva@yahoo.com>
2022-10-22 19:34:21 -0300

    break

--- aula20/aula20.py
+++ aula20/aula20.py
@@ -12,10 +12,10 @@ nova_string = ''
 
 for letra in texto:
     if letra == 't':
-        continue
         nova_string = nova_string + letra.upper()
     elif letra == 'h':
         nova_string += letra.upper()
+        break
     else:
         nova_string += letra
 

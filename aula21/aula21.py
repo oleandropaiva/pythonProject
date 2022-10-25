a45f20b95901b94217965062f529b09d8bceb7c5
Leandro Paiva <leandroapaiva@yahoo.com>
2022-10-24 21:42:11 -0300

    exemplo lista2

--- aula21/aula21.py
+++ aula21/aula21.py
@@ -5,4 +5,12 @@ append, insert, pop, clear, extend, +
 min, max
 range
 """
-lista = [1, 2, 3, 4, 5, 6, 'texto', True, 4.5]
+#         0    1    2    3    4
+lista = ['A', 'B', 'C', 'D', 'E']
+#        -5   -4   -3    -2   -1
+
+string = 'ABCDE'
+
+print(string[1])
+print(lista[1])
+

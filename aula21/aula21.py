ed58cd10b1cf3ba88712a3b14652ac1a580ba8b3
Leandro Paiva <leandroapaiva@yahoo.com>
2022-10-26 21:15:08 -0300

    função pop: remove o ultimo elemento da lista

--- aula21/aula21.py
+++ aula21/aula21.py
@@ -5,11 +5,11 @@ append, insert, pop, clear, extend, +
 min, max
 range
 """
-l1 = [1, 2, 3]
 l2 = [4, 5, 6]
-
-l2.insert(0, 'banana')
-
-print(l1)
 print(l2)
+l2.insert(0, 'banana')
+print(l2)
+l2.pop()
+print(l2)
+
 

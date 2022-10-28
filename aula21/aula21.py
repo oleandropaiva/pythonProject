c3ef074a4c357d690d2ca0289a3e69b8ffbf41f0
Leandro Paiva <leandroapaiva@yahoo.com>
2022-10-27 21:33:40 -0300

    jogo fork em desenv

--- aula21/aula21.py
+++ aula21/aula21.py
@@ -6,10 +6,28 @@ min, max
 range
 """
 secreto = 'perfume'
+digitadas = []
 
 while True:
     letra = input('Digite uma letra: ')
 
     if len(letra) > 1:
-        print('Ahhh isso não vale, digite apenas uma letra.')
+        print('Digite apenas uma letra.')
         continue
+
+    digitadas.append(letra)
+
+    if letra in secreto:
+        print(f'UHUULL, a letra "{letra}" existe na palavra secreta.')
+    else:
+        print(f'A letra"{letra}" Não existe na palavra secreta.')
+        digitadas.pop()
+
+    secreto_temporario = ''
+    for letra_secreta in secreto:
+        if letra_secreta in digitadas:
+            secreto_temporario += letra
+        else:
+            secreto_temporario += '*'
+
+    print(secreto_temporario)

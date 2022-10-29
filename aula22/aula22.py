daca8495864007dc64cb12ec230323aaff367dd5
Leandro Paiva <leandroapaiva@yahoo.com>
2022-10-28 22:38:47 -0300

    if em python

--- aula22/aula22.py
+++ aula22/aula22.py
@@ -2,7 +2,13 @@
 For / Else em Python
 """
 variavel = ['Luiz Otávio', 'João', 'Maria']
+
+comeca_com_j = False
 for valor in variavel:
-    print(valor)
-    break
-    print(valor)
+    if valor.startswith('J'):
+        comeca_com_j = True
+
+    if comeca_com_j:
+        print('Exite uma palavra que começa com J')
+    else:
+        print('Não existe uma palavra que começa com J.')

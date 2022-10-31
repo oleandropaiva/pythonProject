beb8cdea4cffce34bfbe05df4e75c38b08a784c8
Leandro Paiva <leandroapaiva@yahoo.com>
2022-10-31 20:50:08 -0300

    função split, separa com espacos

--- aula23/aula23.py
+++ aula23/aula23.py
@@ -2,5 +2,9 @@
 Split, Join, Enumerate em Python
 * Split - Dividir uma string # str
 * Join - Juntar uma lista # str
-* Enumerate - Enumerar elementos da lista # list
+* Enumerate - Enumerar elementos da lista # list / iteráveis
 """
+string = "O brasil é o país do futebol, o Brasil é penta."
+lista = string.split(' ')
+
+print(lista)

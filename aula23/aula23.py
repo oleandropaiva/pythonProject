2413403712e65077632883f3c660fee212f96a61
Leandro Paiva <leandroapaiva@yahoo.com>
2022-10-31 20:55:05 -0300

    função split, com for

--- aula23/aula23.py
+++ aula23/aula23.py
@@ -4,9 +4,9 @@ Split, Join, Enumerate em Python
 * Join - Juntar uma lista # str
 * Enumerate - Enumerar elementos da lista # list / iteráveis
 """
-string = "O brasil é o país do futebol, o Brasil é penta."
+string = "O Brasil é o país do futebol, o Brasil é penta."
 lista_1 = string.split(' ')
 lista_2 = string.split(',')
 
-print(lista_2)
-print(lista_1)
+for valor in lista_1:
+    print(valor)

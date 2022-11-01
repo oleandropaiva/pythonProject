d001eb4f897be8060f323614bfa7497333586c95
Leandro Paiva <leandroapaiva@yahoo.com>
2022-11-01 17:09:09 -0300

    desempacotamento de lista sem erro com * descrição de item

--- aula24/aula24.py
+++ aula24/aula24.py
@@ -3,6 +3,7 @@ Desenpacotamento de listas em Python
 """
 # atribui  n1      n2      n3
 lista = ['Luiz', 'João', 'Maria', 1, 2, 3, 4]
-n1, n2, * outra_lista = lista
 
-print(n1, n2)
+n1, n2, n3, * outra_lista = lista
+
+print(n1, n2, n3, outra_lista)

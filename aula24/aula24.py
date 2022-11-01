fbd2b44eb04c26748de9c7fa41ce54defb01c7e2
Leandro Paiva <leandroapaiva@yahoo.com>
2022-11-01 17:15:42 -0300

    utilizando *_

--- aula24/aula24.py
+++ aula24/aula24.py
@@ -3,7 +3,6 @@ Desenpacotamento de listas em Python
 """
 # atribui  n1      n2      n3
 lista = ['Luiz', 'João', 'Maria', 1, 2, 3, 4, 5, 6, 7, 8, 100]
+n1, n2, n3, *_, ultimo_da_lista = lista
 
-n1, n2, n3, * outra_lista, ultimo_da_lista = lista
-
-print(outra_lista)
+print(n1)

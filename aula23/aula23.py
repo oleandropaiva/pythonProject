4e6a80ed886e0f69e47c21b1106914520aa9e548
Leandro Paiva <leandroapaiva@yahoo.com>
2022-10-31 21:27:38 -0300

    função .strip(), remove espaços na frase

--- aula23/aula23.py
+++ aula23/aula23.py
@@ -8,13 +8,6 @@ string = "O Brasil é o país país país do futebol, o Brasil é penta."
 lista_1 = string.split(' ')
 lista_2 = string.split(',')
 
-palavra = ''
-contagem = 0
 for valor in lista_2:
-    qtd_vezes = lista_1.count(valor)
+    print(valor.strip())
 
-    if qtd_vezes > contagem:
-        contagem = qtd_vezes
-        palavra = valor
-
-print(f'A palavr que apareceu mais vezes é {palavra} ({contagem}x)')

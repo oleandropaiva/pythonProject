e932e521f2970f47daf0a5fe343d698bb5d4cb39
Leandro Paiva <leandroapaiva@yahoo.com>
2022-10-31 21:04:22 -0300

    função split, com for com count

--- aula23/aula23.py
+++ aula23/aula23.py
@@ -8,5 +8,13 @@ string = "O Brasil é o país do futebol, o Brasil é penta."
 lista_1 = string.split(' ')
 lista_2 = string.split(',')
 
+palavra = ''
+contagem = 0
 for valor in lista_1:
-    print(f'A palavra {valor} apareceu {lista_1.count(valor)} x na frase')
+    qtd_vezes = lista_1.count(valor)
+
+    if qtd_vezes > contagem:
+        contagem = qtd_vezes
+        palavra = valor
+
+print(f'A palavre que apareceu mais vezes é {palavra} ({contagem}x)')

98b0ce603bb28a40cbb26304242544df21197f1e
Leandro Paiva <leandroapaiva@yahoo.com>
2022-11-01 17:11:31 -0300

    desempacotamento-imprime ultimo item da lista

--- aula24/aula24.py
+++ aula24/aula24.py
@@ -2,8 +2,8 @@
 Desenpacotamento de listas em Python
 """
 # atribui  n1      n2      n3
-lista = ['Luiz', 'João', 'Maria', 1, 2, 3, 4]
+lista = ['Luiz', 'João', 'Maria', 1, 2, 3, 4, 5, 6, 7, 8, 100]
 
-n1, n2, n3, * outra_lista = lista
+n1, n2, n3, * outra_lista, ultimo_da_lista = lista
 
-print(n1, n2, n3, outra_lista)
+print(ultimo_da_lista)

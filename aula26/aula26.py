84fd7c67542947c8b4874e13a14c2269eadcd7fb
Leandro Paiva <leandroapaiva@yahoo.com>
2022-11-02 23:55:37 -0300

    operador ternário em Python

--- aula26/aula26.py
+++ aula26/aula26.py
@@ -1,11 +1,7 @@
 """
 Operador ternário em Python
 """
-logged_user = False
-
-if logged_user:
-    msg = 'Usuário logado.'
-else:
-    msg = 'Usuário precisa logar.'
+logged_user = True
+msg = 'Usuário logado.' if logged_user else 'Usuário precisa logar.'
 
 print(msg)

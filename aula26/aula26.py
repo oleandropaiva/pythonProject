92e5ef5cc5a4732a494168f0b12b2741e582b43e
Leandro Paiva <leandroapaiva@yahoo.com>
2022-11-03 00:03:56 -0300

    exemplo de operador ternário em Python

--- aula26/aula26.py
+++ aula26/aula26.py
@@ -1,8 +1,13 @@
 """
 Operador ternário em Python
 """
-idade = 18
-e_maior = (idade >=18)
-msg = 'Pode acessar' if e_maior else 'Não pode acessar'
+idade = input('Qual a sua idade? ')
+
+if not idade.isnumeric():
+    print('Digite apenas números')
+else:
+    idade = int(idade)
+    e_maior = (idade >= 18)
+    msg = 'Pode acessar' if e_maior else 'Não pode acessar'
 
 print(msg)

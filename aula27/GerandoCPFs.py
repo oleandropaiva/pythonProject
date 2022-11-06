381435353d8061c6a3a6a1438de4ace50ffeafd3
Leandro Paiva <leandroapaiva@yahoo.com>
2022-11-05 23:44:33 -0300

    escopo

---
+++ aula27/GerandoCPFs.py
@@ -0,0 +1,6 @@
+from random import randint
+numero = str(randint(100000000, 999999999))
+
+novo_cpf = numero                   # 9 números aleatórios
+reverso = 10                        # Contador reverso
+total = 0                           # O total das multiplicações

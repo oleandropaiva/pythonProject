a6743a6c6a4bacd73d8a5cc841d71304eae49cff
Leandro Paiva <leandroapaiva@yahoo.com>
2022-11-14 19:47:05 -0300

    Desenpacotamento de listas em Python

--- aula24/aula24.py
+++ aula24/aula24.py
@@ -1,7 +1,6 @@
 """
 Desenpacotamento de listas em Python
 """
-# atribui  n1      n2      n3
 lista = ['Luiz', 'João', 'Maria', 1, 2, 3, 4, 5, 6, 7, 8, 100]
 n1, n2, n3, *_, ultimo_da_lista = lista
 
